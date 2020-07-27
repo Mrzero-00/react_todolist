0b29008844312c70db5de32ed8553f4231ec70e5
sangi_imac <mrzero885@gmail.com>
2020-07-27 15:34:14 +0900

    Updates

---
+++ precache-manifest.2046cc92fe121a24e7d1710cf59a565c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5038de75cdafcb1408e87087eb22e759",
+    "url": "/react_gogo/index.html"
+  },
+  {
+    "revision": "42e0c79580455ab00ce2",
+    "url": "/react_gogo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a7c3dd6a76cf5b3b8926",
+    "url": "/react_gogo/static/js/2.8b400f44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_gogo/static/js/2.8b400f44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42e0c79580455ab00ce2",
+    "url": "/react_gogo/static/js/main.4ffd0b82.chunk.js"
+  },
+  {
+    "revision": "ec63c549a16a20488cdf",
+    "url": "/react_gogo/static/js/runtime-main.859a5edf.js"
+  }
+]);
